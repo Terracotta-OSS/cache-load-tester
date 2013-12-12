76e2c964a61a9e5932bd8e6a38be02e8318101d1
abroszni <abroszni@terracottatech.com>
2013-12-12 10:19:06 +0000

    Beginner's mistake, adding missing class

---
+++ src/main/java/org/terracotta/ehcache/testing/cache/CacheWriterWrapper.java
@@ -0,0 +1,35 @@
+package org.terracotta.ehcache.testing.cache;
+
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.constructs.nonstop.NonStopCacheException;
+import org.terracotta.ehcache.testing.statistics.Stats;
+
+/**
+ * @author Aurelien Broszniowski
+ */
+public class CacheWriterWrapper extends CacheWrapperImpl {
+  /**
+   * Implementation of {@link org.terracotta.ehcache.testing.cache.CacheWrapper}
+   *
+   * @param cache the underlying Ehcache
+   */
+  public CacheWriterWrapper(final Ehcache cache) {
+    super(cache);
+  }
+
+  @Override
+  public void put(final Object key, final Object value) {
+    System.out.println("------------ PUT");
+    long start = (statistics) ? now() : 0;
+    try {
+      cache.put(new Element(key, value));
+    } catch (NonStopCacheException nsce) {
+      writeStats.incrementNonstopExceptionCount();
+    }
+    long end = (statistics) ? now() : 0;
+    if (statistics) {
+      writeStats.add(end - start);
+    }
+  }
+}
