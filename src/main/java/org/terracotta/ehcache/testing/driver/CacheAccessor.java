b02b90562ec550e4bfe8748c6ced4d7505460c92
sbansal <sbansal@terracottatech.com>
2014-01-08 13:00:13 +0000

    Reverting 34016.

--- src/main/java/org/terracotta/ehcache/testing/driver/CacheAccessor.java
+++ src/main/java/org/terracotta/ehcache/testing/driver/CacheAccessor.java
@@ -138,14 +138,6 @@ public abstract class CacheAccessor implements CacheDriver {
   public abstract CacheAccessor validateUsing(final Validation.Mode validationMode, Validation validation);
 
   /**
-   * Enable performance statistics reporting on console
-   *
-   * @param statistics
-   * @return this
-   */
-  public abstract CacheAccessor enableStatReporting(boolean statistics);
-  
-  /**
    * Enable statistics collection
    *
    * @param statistics
@@ -486,13 +478,8 @@ public abstract class CacheAccessor implements CacheDriver {
 	}
 
 	@Override
-	public CacheAccessor enableStatReporting(boolean enabled) {
-		this.statistics = enabled;
-		return this;
-	}
-	
-	@Override
 	public CacheAccessor enableStatistics(boolean enabled) {
+		this.statistics = enabled;
 		cacheWrapper.setStatisticsEnabled(enabled);
 		return this;
 	}
@@ -721,15 +708,10 @@ public abstract class CacheAccessor implements CacheDriver {
 		individualCacheAccessor.removeRatio(removeRatio);
 	  return this;
 	}
-	
-	@Override
-	public CacheAccessor enableStatReporting(boolean enabled) {
-		this.statistics = enabled;
-		return this;
-	}
 
 	@Override
 	public CacheAccessor enableStatistics(boolean statistics) {
+		this.statistics = statistics;
 		for (IndividualCacheAccessor accessor : accessors)
 			accessor.enableStatistics(statistics);
 	  return this;
--- src/main/java/org/terracotta/ehcache/testing/driver/CacheLoader.java
+++ src/main/java/org/terracotta/ehcache/testing/driver/CacheLoader.java
@@ -183,12 +183,8 @@ public class CacheLoader implements CacheDriver {
   public CacheLoader enableStatistics(boolean enabled) {
     for (CacheWrapper cache : caches)
       cache.setStatisticsEnabled(enabled);
+    this.statistics = enabled;
     return this;
-  }
-  
-  public CacheLoader enableStatReporting(boolean enabled) {
-	    this.statistics = enabled;
-	    return this;
   }
 
   public CacheDriver fillPartitioned(long count, int threads) {
